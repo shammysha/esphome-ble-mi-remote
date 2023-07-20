cf4b713068a0f7e673aa8ecc3e4ac2a32249a44a
Maxim Sharenkov <mcshaman@gmail.com>
2023-07-20 10:11:30 +0300

    Power advertise

--- components/ble_mi_remote/ble_mi_remote.cpp
+++ components/ble_mi_remote/ble_mi_remote.cpp
@@ -230,7 +230,7 @@ namespace esphome {
 		}
 
 		void BleMiRemote::powerAdvertisingStart() {
-			powerAdvertising->start(1000, void((NimBLEAdvertising*) this->powerAdvertisingStop) );
+			powerAdvertising->start(1000, (NimBLEAdvertising*) this->powerAdvertisingStop) );
 		}
 
 		static void BleMiRemote::powerAdvertisingStop() {
