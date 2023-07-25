7074f7bc2c04a7aa4cb639a1ef79bd40d373bd12
Maxim Sharenkov <mcshaman@gmail.com>
2023-07-25 08:31:35 +0300

    Power advertising

--- components/ble_mi_remote/ble_mi_remote.h
+++ components/ble_mi_remote/ble_mi_remote.h
@@ -88,7 +88,7 @@ namespace esphome {
 
 				void powerAdvertisingSetup();
 				void powerAdvertisingStart();
-				advCompleteCB* powerAdvertisingStop(NimBLEAdvertising *pAdv);
+				void powerAdvertisingStop(NimBLEAdvertising *pAdv);
 
 				void vendorServicesSetup();
 
