0cb927e896708424098840d1af773ce2e80eabd6
Maxim Sharenkov <mcshaman@gmail.com>
2023-07-27 15:54:32 +0300

    Power advertise

--- components/ble_mi_remote/ble_mi_remote.cpp
+++ components/ble_mi_remote/ble_mi_remote.cpp
@@ -320,7 +320,7 @@ namespace esphome {
 			ESP_LOGD(TAG, "Power payload is:");
 			ESP_LOGD(TAG, powerAdvData->getPayload().c_str());
 
-			powerAdvertising->start(1, [ this ](powerAdvertising *pAdv) { advCompleteCB(pAdv); });
+			powerAdvertising->start(1, [ this ](NimBLEAdvertising *pAdv) { advCompleteCB(pAdv); });
 		}
 
 		void BleMiRemote::advCompleteCB(NimBLEAdvertising *pAdv) {
