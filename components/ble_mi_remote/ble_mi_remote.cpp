ae3d23aa0c080672568feb5c7e4c5641a9852645
NAGL95 <n.a.gl@yandex.ru>
2025-12-14 02:06:29 +0300

    Скорректировал строчки 491-497

--- components/ble_mi_remote/ble_mi_remote.cpp
+++ components/ble_mi_remote/ble_mi_remote.cpp
@@ -489,14 +489,11 @@ namespace esphome {
 		}
 
 		void BleMiRemote::onDisconnect(NimBLEServer *pServer, NimBLEConnInfo& connInfo, int reason) {
-<<<<<<< HEAD
 		  ESP_LOGD(TAG, "onDisconnect FIRED!!!");
-=======
->>>>>>> branch 'main' of https://github.com/shammysha/esphome-ble-mi-remote
 		  this->_connected = false;
-			if (this->_reconnect) {
-			  pServer->startAdvertising();
-			}
+		  if (this->_reconnect) {
+		    pServer->startAdvertising();
+		  }
 		}
 
 		void BleMiRemote::onWrite(NimBLECharacteristic *me, NimBLEConnInfo& connInfo) {
