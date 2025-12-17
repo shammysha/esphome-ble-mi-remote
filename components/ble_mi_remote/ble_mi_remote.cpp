aa10c14545efde6d26c16a04c4eeadeca7e97283
Maxim Sharenkov <mcshaman@gmail.com>
2025-12-17 05:24:44 +0300

    Fix pServer initialization

--- components/ble_mi_remote/ble_mi_remote.cpp
+++ components/ble_mi_remote/ble_mi_remote.cpp
@@ -151,7 +151,7 @@ namespace esphome {
 			ESP_LOGI(TAG, "Setting this up...");
 
 			NimBLEDevice::init(deviceName);
-			NimBLEDevice *pServer = NimBLEDevice::createServer();
+			NimBLEServer* pServer = NimBLEDevice::createServer();
 
 			pServer->setCallbacks(this);
       		pServer->advertiseOnDisconnect(this->_reconnect);
